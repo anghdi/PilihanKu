ada8f0339ac86a32969074bf337f6dfa8de9478d
Angga Hadi Permana <anggahadi447@gmail.com>
2024-09-26 21:57:18 +0800

    up1

--- pilihanku-v1/app/Views/admin/auth/LoginAdmin.php
+++ pilihanku-v1/app/Views/admin/auth/LoginAdmin.php
@@ -8,58 +8,40 @@
     <link href="<?= base_url('css/output.css') ?>" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="<?= base_url('images/vote.png') ?>">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <style>
+        
+    </style>
 </head>
 
 <body>
-    <main class="w-full flex flex-row justify-between">
-        <div class="w-4/6 h-screen flex justify-center items-center">
-            <img src="<?= base_url('images/login.jpg') ?>" alt="" srcset="" class="h-full">
+    <nav class="navbar top-0">
+        <div class="navbar-center text-center mx-auto border-4 lg:px-96 px-20 py-3 rounded-2xl bg-slate-200">
+            <h1 class="font-bold text-3xl sm:text-4xl tracking-wider">PILIHANKU</h1>
         </div>
-        <div class="w-2/6 flex justify-center items-center h-screen bg-slate-200">
-            <?php if (isset($validation)): ?>
-                <div class="absolute top-44 bg-red-400 rounded-md py-2 px-4 text-white font-medium">
-                    <!-- Tampilkan semua pesan error -->
-                    <?= implode('<br>', $validation) ?>
-                </div>
-            <?php endif; ?>
-            <?php if (session()->getFlashdata('error')): ?>
-                <div class="absolute top-44 bg-red-400 rounded-md py-2 px-4 text-white font-medium flex flex-row justify-center items-center">
-                    <span class="material-symbols-outlined px-2">
-                        mood_bad
-                    </span>
-                    <?= session()->getFlashdata('error') ?>
-                </div>
-            <?php endif; ?>
-            <h1 class="top-20 text-6xl font-bold absolute">
-                Pilihanku
-            </h1>
-            <form action="/login" method="post" class="flex flex-col w-3/4">
-                <?= csrf_field() ?>
-                <div class="flex flex-row justify-center items-center gap-2 ">
+    </nav>
+    <main class="top-0 hero bg-base-200 h-screen">
+        <div class="flex flex-col lg:flex-row lg:justify-between">
+            <div class="glass mx-auto bg-yellow-500 rounded-2xl lg:w-5/12 ">
+                <dotlottie-player src="https://lottie.host/95b7605f-e505-4b3f-8065-0472f2caca83/rbz3UfwIhx.json" background="transparent" speed="1" class="mx-auto" loop autoplay></dotlottie-player>
+            </div>
+            <form action="" method="post" class="mx-auto py-4 flex flex-col justify-center items-center gap-5 border-2 lg:w-2/6">
+                <div class="flex justify-center items-center flex-row gap-2 lg:w-1/2">
                     <span class="material-symbols-outlined">
                         person
                     </span>
-                    <input type="text"
-                        class="mt-0 block w-full px-0.5 border-0 border-b-2 border-blue-300 bg-slate-200 focus:ring-0 focus:border-blue"
-                        placeholder="Masukkan Username" name="username" value="<?= set_value('username') ?>" required>
+                    <input type="text" class="mt-0 block w-full px-0.5 border-0 border-b-2 bg-base-200 focus:ring-0 border-black " placeholder="Masukkan Username">
                 </div>
-                <div class="flex flex-row justify-center items-center gap-2 py-10">
+                <div class="flex justify-center items-center flex-row gap-2 lg:w-1/2">
                     <span class="material-symbols-outlined">
                         lock
                     </span>
-                    <input type="password" name="password" id=""
-                        class="mt-0 block w-full px-0.5 border-0 border-b-2 border-blue-300 bg-slate-200 focus:ring-0 focus:border-blue"
-                        placeholder="Masukkan Password" required>
+                    <input type="password" class="mt-0 block w-full px-0.5 border-0 border-b-2 bg-base-200 focus:ring-0 border-black" placeholder="Masukkan Password">
                 </div>
-                <button type="submit"
-                    class="border-2 bg-slate-600 rounded-md text-white font-semibold hover:bg-slate-900 py-2 mt-5">
-                    Login
-                </button>
-                <span class="font-light mt-3"> Belum punya akun ? <a href="#" class="text-blue-500 font-medium"> Daftar
-                        disini </a> </span>
+                <button type="submit" class="btn btn-square btn-warning px-12 "> <span class="text-lg font-medium">Login</span></button>
             </form>
         </div>
     </main>
+    <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
 </body>
 
 </html>
